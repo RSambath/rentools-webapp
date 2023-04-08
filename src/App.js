1ca1bfaa20132ede397ff636b8d490973c290c5d
RSambath <63772144+RSambath@users.noreply.github.com>
2023-04-09 00:46:26 +0700

    removed used imports and hero.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import About from "./pages/About";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
-import { Route, Link, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import HeroImage from "../components/HeroImage"; 
-import Footer from "../components/Footer"; 
-import login from "./Login";
-import { Route, Link } from "react-router-dom";
+import HeroImage from "../components/HeroImage";
+import Footer from "../components/Footer";
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <Hero />
       <HeroImage/>
       <Footer/>
     </div>
