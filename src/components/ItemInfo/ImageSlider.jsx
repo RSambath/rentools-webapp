9138b9acc2b7133a29f259bddab25e22284dc101
Hong LY <97499775+HongLY2020181@users.noreply.github.com>
2023-05-07 19:30:03 +0700

    Commit

--- src/components/ItemInfo/ImageSlider.jsx
+++ src/components/ItemInfo/ImageSlider.jsx
@@ -2,21 +2,6 @@ import React from 'react';
 import ImageGallery from 'react-image-gallery'; //npm i react-image-gallery
 import '../../index.css';
 
-const images = [
-  {
-    original: 'https://picsum.photos/id/1018/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1018/250/150/',
-  },
-  {
-    original: 'https://via.placeholder.com/150',
-    thumbnail: 'https://via.placeholder.com/150',
-  },
-  {
-    original: 'https://picsum.photos/id/1019/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1019/250/150/',
-  }
-];
-
 function Item(props) {
   const image = props.images;
 
